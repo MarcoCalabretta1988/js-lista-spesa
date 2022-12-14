c24db7e1f931bd841b89aa4e9de4f0a8ae3caa54
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2022-12-14 14:51:24 +0100

    End version template literals add style

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 // CHECK JS LINK
 console.log('JS OK');
+
+//! TESTO 
+
+/*Data una lista della spesa , 
+stampare in pagina gli elementi della lista individualmente con un ciclo while.*/
+
+//Creo lista della spesa
+
+const groceryList = ['peperoni','mele','carne','formaggio','pasta','biscotti',];
+
+//Recupero elemento dal DOM
+
+const groceryElement = document.getElementById('grocery-list');
+
+//Variabile contatore
+let i = 0;
+let message = '<ul>';
+
+while ( i < groceryList.length){
+ message += `<li>${groceryList[i]}</li>`;
+ i++;
+}
+
+message += '</ul>'
+
+groceryElement.innerHTML = message;
